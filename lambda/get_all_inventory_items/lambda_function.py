bdc33e9d0c50888c63cf84e752596522258928ea
Navraj Singh <navrajsingh@rrc.ca>
2025-11-24 16:18:04 -0600

    Create get_all_items lambda_function.py

---
+++ lambda/get_all_inventory_items/lambda_function.py
@@ -0,0 +1,19 @@
+import json
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('Inventory')
+
+def lambda_handler(event, context):
+    try:
+        response = table.scan()
+        return {
+            'statusCode': 200,
+            'body': json.dumps(response['Items'])
+        }
+
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
+        }
